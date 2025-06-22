555f38dc98268f90905ca97d448170237947ff7c
Shiido1 <uwahsheedo@gmail.com>
2025-06-23 00:35:16 +0100

    commit work almost done

--- lib/common/io/fetch_data.dart
+++ lib/common/io/fetch_data.dart
@@ -11,3 +11,5 @@ Future<dynamic> fetchDAta() async {
 }
 
 
+
+
--- lib/modules/home/presentation/home_screen.dart
+++ lib/modules/home/presentation/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:mobile_developer_assessment/common/io/fetch_data.dart';
 import '../../../common/io/shared_preferance.dart';
 import '../../widgets/employee_ticket_widget.dart';
 import '../../widgets/text_form_widget.dart';
+import '../../widgets/text_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,25 +19,52 @@ class _HomeScreenState extends State<HomeScreen> {
   late Future<dynamic> futureData;
   String _query = '';
   final session = SharedPreferencesService();
+  bool isFilter = false;
+
+  List<dynamic> modelList = [];
 
   @override
   void initState() {
     fetchResponseData();
+    onSetDataPreference();
     super.initState();
   }
 
-  onSetDataPreference() {
-    Future.delayed(Duration(seconds: 3), () {
+  onSetDataPreference() async {
+    await Future.delayed(Duration(seconds: 3), () {
       setState(() {});
+      getDesignation(session.usersData['userData']);
     });
+    return;
   }
 
   @override
   Widget build(BuildContext context) {
-    onSetDataPreference();
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(backgroundColor: const Color.fromARGB(255, 196, 221, 199)),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 196, 221, 199),
+        actions: [
+          isFilter
+              ? Padding(
+                padding: const EdgeInsets.only(right:8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    modelList.clear();
+                    isFilter = false;
+                    setState(() {});
+                  },
+                  child: TextView(
+                    text: 'Clear',
+                    fontSize: 15.90,
+                    fontWeight: FontWeight.w600,
+                    color: const Color.fromARGB(255, 17, 47, 18),
+                  ),
+                ),
+              )
+              : SizedBox.shrink(),
+        ],
+      ),
       body: Column(
         children: [
           Padding(
@@ -52,14 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
               fillColor: Colors.white.withOpacity(.4),
               isFilled: true,
               prefixWidget: Icon(Icons.search),
-              suffixWidget: GestureDetector(
-                onTap: () {},
-                child: Icon(
-                  Icons.format_list_bulleted_rounded,
-                  size: 30,
-                  color: Colors.green,
-                ),
-              ),
+              suffixWidget: selectPopUpMenu(context, designation: designation),
               onChange: (o) {
                 _query = o;
                 setState(() {});
@@ -75,23 +96,69 @@ class _HomeScreenState extends State<HomeScreen> {
                   if (SharedPreferencesService.instance.usersData['userData'] !=
                       null)
                     if (_query != '')
-                      ...session.usersData['userData']
-                          .where((w) {
-                            final firstName =
-                                w['first_name']?.toString().toLowerCase() ?? '';
-                            final lastName =
-                                w['last_name']?.toString().toLowerCase() ?? '';
-                            final query = _query.toLowerCase();
-                            return firstName.contains(query) ||
-                                lastName.contains(query);
-                          })!
-                          .map((o) {
-                            return EmployeeTicketWidget(o: o);
-                          })
+                      !isFilter
+                          ? Column(
+                            children: [
+                              ...session.usersData['userData']
+                                  .where((w) {
+                                    final firstName =
+                                        w['first_name']
+                                            ?.toString()
+                                            .toLowerCase() ??
+                                        '';
+                                    final lastName =
+                                        w['last_name']
+                                            ?.toString()
+                                            .toLowerCase() ??
+                                        '';
+                                    final query = _query.toLowerCase();
+                                    return firstName.contains(query) ||
+                                        lastName.contains(query);
+                                  })!
+                                  .map((o) {
+                                    return EmployeeTicketWidget(o: o);
+                                  }),
+                            ],
+                          )
+                          : Column(
+                            children: [
+                              ...modelList
+                                  .where((w) {
+                                    final firstName =
+                                        w['first_name']
+                                            ?.toString()
+                                            .toLowerCase() ??
+                                        '';
+                                    final lastName =
+                                        w['last_name']
+                                            ?.toString()
+                                            .toLowerCase() ??
+                                        '';
+                                    final query = _query.toLowerCase();
+                                    return firstName.contains(query) ||
+                                        lastName.contains(query);
+                                  })
+                                  .map((o) {
+                                    return EmployeeTicketWidget(o: o);
+                                  }),
+                            ],
+                          )
                     else
-                      ...session.usersData['userData']!.map((o) {
-                        return EmployeeTicketWidget(o: o);
-                      })
+                      !isFilter
+                          ? Column(
+                            children: [
+                              ...session.usersData['userData']!.map((o) {
+                                return EmployeeTicketWidget(o: o);
+                              }),
+                            ],
+                          )
+                          : Column(
+                            children: [
+                              ...modelList.map((o) {
+                                return EmployeeTicketWidget(o: o);
+                              }),
+                            ],
+                          )
                   else
                     Center(
                       child: CircularProgressIndicator(
@@ -108,6 +175,19 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  List<String> designation = [];
+
+  List<String> getDesignation(itemList) {
+    designation.clear();
+    for (var element in itemList) {
+      if (designation.contains(element['designation'])) {
+      } else {
+        designation.add(element['designation']);
+      }
+    }
+    return designation;
+  }
+
   void _chache(data) {
     if (data != null) {
       session.usersData = {'userData': data['data']};
@@ -119,4 +199,43 @@ class _HomeScreenState extends State<HomeScreen> {
     _chache(v);
     setState(() {});
   }
+
+  selectPopUpMenu(context, {List<String>? designation}) =>
+      PopupMenuButton<String>(
+        itemBuilder:
+            (context) => [
+              if (designation!.isNotEmpty)
+                ...designation.map(
+                  (e) => PopupMenuItem(
+                    value: e,
+                    child: TextView(
+                      text: e,
+                      fontSize: 15.90,
+                      fontWeight: FontWeight.w600,
+                      color: const Color.fromARGB(255, 17, 47, 18),
+                    ),
+                  ),
+                ),
+            ],
+        offset: const Offset(0, 100),
+        color: const Color.fromARGB(255, 253, 253, 253),
+        elevation: 2,
+        // on selected we show the dialog box
+        onSelected: (value) {
+          modelList.clear();
+          isFilter = true;
+
+          for (var emp in session.usersData['userData']) {
+            if (emp['designation'] == value) {
+              modelList.add(emp);
+            }
+          }
+          setState(() {});
+        },
+        child: Icon(
+          Icons.format_list_bulleted_rounded,
+          size: 30,
+          color: Colors.green,
+        ),
+      );
 }
